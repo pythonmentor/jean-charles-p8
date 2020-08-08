8d1d077f2df9a69cfc44e608a04c074fb1b81885
jean-charles-gibier <jeancharlesg@free.fr>
2020-08-08 17:59:41 +0200

    Pb cache Ci

--- pur_beurre/user/views.py
+++ pur_beurre/user/views.py
@@ -12,15 +12,13 @@ def dashboard(request):
     return render(request, "user/dashboard.html")
 
 
-
 def user(request):
     """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
     return HttpResponse("""
         <h1>Bienvenue sur lapage des users !</h1>
         <p>Les utilisateurs quoi !</p>
     """)
-    
-        
+            
 
 def register(request):
     if request.method == "GET":
